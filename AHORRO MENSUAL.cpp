de95493146f47090250f043bb99b23a789d41f89
Helen <lissa.acevedo99@gmail.com>
2021-09-18 08:49:39 -0600

    Agregue archivos AH. Y ARREGLO

---
+++ AHORRO MENSUAL.cpp
@@ -0,0 +1,40 @@
+//AHORRO MENSUAL
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	
+	
+	
+float var1,montoacum,ahrmen;
+char name;
+
+
+cout << "***********Ahorro mensual************** ";
+
+cout <<"\n"<<endl;
+
+
+
+for (int i = 0; i < 12; i++);
+ {
+  cout <<"Ingrese el monto que desea ahorrarr Q. " ;
+   cin >>ahrmen;
+    
+    cout<<" Saldo acumulado mensual Q. "<< ahrmen <<".00.";
+
+ }
+ 
+
+
+return 0;
+	
+	
+	
+}
+
